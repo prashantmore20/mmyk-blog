e92d89d669e192f14bfd15a718e98e966feab70f
prashantmore20 <prashant.more20@gmail.com>
2021-01-24 14:24:51 +0530

    login page designed and css applied

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,9 @@ from . import views
 # Create your views here.
 
 def blogHome(request):
-    return HttpResponse ('This is Blog Home Page')
+    # return HttpResponse ('This is Blog Home Page')
+    return render (request, 'blog/blogHome.html')
 
 def blogPost(request, slug):
-    return HttpResponse (f'This is Blog Home Page: {slug}')
+    return render (request, 'blog/blogPost.html')
+    # return HttpResponse (f'This is Blog Home Page: {slug}')
